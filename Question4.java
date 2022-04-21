3a0dcdefd83744e78d799a1a6ed4dc9bf75096e4
jobychong <94032188+jobychong@users.noreply.github.com>
2022-04-21 10:37:54 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -4,18 +4,17 @@ public class Question4
 {
   public static void main(String[] args)
   {
-    int i,j,rows;
- Scanner sc = new Scanner(System.in);
-     System.out.print("Enter the number of rows: ");
-     rows = sc.nextInt();
-    
- for(i=1;i<=rows;i++)
-     {
-         for(j=1;j<=i;j++)
-         {
-             System.out.print("* ");
-         }
-         System.out.println("");
-     } 
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter the number of rows: ");
+    int rows = in.nextInt();
+
+    for(int i=rows;0<i;i--)
+    {
+       for(int j=0;j<i;j++)
+       {
+         System.out.print("* ");
+       }
+    System.out.println();
+    } 
   }
 }
